f1a4ab2d5ac12588ac5c5632d8a86d0cad2551d6
Casey Ann Cumbow <cumbow8@gmail.com>
2016-09-01 15:14:00 -0600

    added my routes!!

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :dogs do
+    resources :cats do
+      resources :canaries
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
